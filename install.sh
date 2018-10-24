2bd16ef2780794707ddc6b5aa6275a5fdf841a6d
songlin <songlin.lin@yunfangdata.com>
2018-10-24 10:15:22 +0800

    增加安装记录

--- install.sh
+++ install.sh
@@ -39,4 +39,5 @@ certutil -d sql:$HOME/.pki/nssdb -A -t C -n mitmproxy -i ~/.mitmproxy/mitmproxy-
 
 # 安装zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+# 安装CloudClip
+#git clone https://github.com/skywind3000/CloudClip.git
