56922e9ee6f89ee26260a0b4c7d67797d8c00134
lin <songlin.lin@yunfangdata.com>
2019-05-20 14:20:01 +0800

    增加linux brew 安装

--- install.sh
+++ install.sh
@@ -165,3 +165,6 @@ cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+# 安装linux brew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
