37ca3382accde9473c072bde441bf4380dbcbc68
lin <linsl2018@163.com>
2019-07-19 15:29:11 +0800

    增加安装java开发环境的方法

--- install.sh
+++ install.sh
@@ -181,4 +181,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update
-sudo dnf install code
+sudo dnf install code -y
+# 安装openjdk开发环境
+# http://openjdk.java.net/install/
+sudo dnf install java-1.8.0-openjdk-devel -y
