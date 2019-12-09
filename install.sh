071955a3f121286a27f33b39c398fdf0b27990ca
lin <linsl2018@163.com>
2019-12-09 14:47:24 +0800

    增加git clone 走代理配置

--- install.sh
+++ install.sh
@@ -195,3 +195,6 @@ chmod +x ./trans
 sudo mv trans /usr/local/bin/
 
 brew install onefetch
+
+git config --global http.proxy 'socks5://127.0.0.1:1080'
+git config --global https.proxy 'socks5://127.0.0.1:1080'
