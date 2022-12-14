5b802055bcff0261d0fe7cc8c9bc5bac69b67e99
linsonglin <linsonglin@roboeye.ai>
2022-12-14 16:38:32 +0800

    update auto unlock.sh

--- auto_unlock.sh
+++ auto_unlock.sh
@@ -10,12 +10,15 @@ xset s off
 while true;do
 	echo "check"
 	if ping -c 1 192.168.0.35; then
-		echo "unlock"
-		gnome-screensaver-command -d
-	else
-		echo "lock"
-		gnome-screensaver-command -l
+		# https://unix.stackexchange.com/questions/120153/resolving-mac-address-from-ip-address-in-linux
+		if arp -a | grep 192.168.0.35 | grep 34:2e:b6:a3:82:6b; then
+			echo "unlock"
+			gnome-screensaver-command -d
+			sleep 30
+			continue
+		fi
 	fi
-
-	sleep 60
+	echo "lock"
+	gnome-screensaver-command -l
+	sleep 30
 done
